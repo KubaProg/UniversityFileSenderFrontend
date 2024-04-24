8d60d025ba0680875dddde2d28c69f7821e38396
Jakub_Opielka <jakub.opielka@q-perior.com>
2024-04-24 23:38:21 +0200

    added register form and configured the special colors for material

---
+++ src/app/start-screen/start/start.component.ts
@@ -0,0 +1,28 @@
+import {Component, Inject} from '@angular/core';
+import { MatCardModule } from '@angular/material/card';
+import { MatButtonModule } from '@angular/material/button';
+import {Router} from "@angular/router";
+import {LoginComponent} from "../../login-screen/login/login.component";
+import {MatIcon} from "@angular/material/icon";
+
+@Component({
+  selector: 'app-start',
+  standalone: true,
+  imports: [
+    MatCardModule,
+    MatButtonModule,
+    MatIcon
+  ],
+  templateUrl: './start.component.html',
+  styleUrls: ['./start.component.scss']
+})
+export class StartComponent {
+  constructor(private router: Router) { }
+
+  navigateLogin() {
+    this.router.navigate(['login']);
+  }
+  navigateRegister() {
+    this.router.navigate(['register'])
+  }
+}
