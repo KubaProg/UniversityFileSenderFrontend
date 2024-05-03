a8051e397299c9da2b15776171c4e7ac2ca2dfa4
Jakub_Opielka <jakub.opielka@q-perior.com>
2024-05-03 21:34:46 +0200

    moje plecy wentyluje nowy system w audi a8

--- src/app/main-panel/shared/course-element-short/course-element-short.component.ts
+++ src/app/main-panel/shared/course-element-short/course-element-short.component.ts
@@ -1,14 +1,20 @@
 import {Component, Input} from '@angular/core';
+import {MatButton} from "@angular/material/button";
+import {NgIf} from "@angular/common";
 
 @Component({
   selector: 'app-course-element-short',
   standalone: true,
-  imports: [],
+  imports: [
+    MatButton,
+    NgIf
+  ],
   templateUrl: './course-element-short.component.html',
   styleUrl: './course-element-short.component.scss'
 })
 export class CourseElementShortComponent {
 
   @Input() course = 'Course Name'
+  @Input() isStudent = false;
 
 }
