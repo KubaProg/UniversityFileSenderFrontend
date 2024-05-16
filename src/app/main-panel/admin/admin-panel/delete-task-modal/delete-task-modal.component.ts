f0b7553eeb8ff9560f3919e4d45d59ceb4f9023b
Jakub_Opielka <jakub.opielka@q-perior.com>
2024-05-16 20:05:53 +0200

    added modals for edit and delte task

---
+++ src/app/main-panel/admin/admin-panel/delete-task-modal/delete-task-modal.component.ts
@@ -0,0 +1,42 @@
+import {Component, Inject} from '@angular/core';
+import {FormsModule} from "@angular/forms";
+import {MatButton} from "@angular/material/button";
+import {
+  MAT_DIALOG_DATA,
+  MatDialogActions,
+  MatDialogClose,
+  MatDialogContent,
+  MatDialogRef
+} from "@angular/material/dialog";
+import {MatFormField, MatLabel} from "@angular/material/form-field";
+import {MatInput} from "@angular/material/input";
+import {addCourseDialogData, addTaskDialogData} from "../admin-panel.component";
+
+@Component({
+  selector: 'app-delete-task-modal',
+  standalone: true,
+  imports: [
+    FormsModule,
+    MatButton,
+    MatDialogActions,
+    MatDialogContent,
+    MatFormField,
+    MatInput,
+    MatLabel,
+    MatDialogClose
+  ],
+  templateUrl: './delete-task-modal.component.html',
+  styleUrl: './delete-task-modal.component.scss'
+})
+export class DeleteTaskModalComponent {
+
+  constructor(
+    public dialogRef: MatDialogRef<DeleteTaskModalComponent>,
+    @Inject(MAT_DIALOG_DATA) public data: addTaskDialogData
+  ) {}
+
+  onCancelCLick(): void {
+    this.dialogRef.close();
+  }
+
+}
---
+++ src/app/main-panel/admin/admin-panel/edit-task-modal/edit-task-modal.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-edit-task-modal',
+  standalone: true,
+  imports: [],
+  templateUrl: './edit-task-modal.component.html',
+  styleUrl: './edit-task-modal.component.scss'
+})
+export class EditTaskModalComponent {
+
+}
