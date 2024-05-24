2725f7a1d487c8d6818e62f5905e89da66075686
Jakub_Opielka <jakub.opielka@q-perior.com>
2024-05-24 19:11:49 +0200

    made the design good

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,7 +1,6 @@
 import { Routes } from '@angular/router';
 import {StartComponent} from "./start-screen/start/start.component";
 import {LoginComponent} from "./login-screen/login/login.component";
-import {RegisterComponent} from "./register-screen/register/register.component";
 import {AdminPanelComponent} from "./main-panel/admin/admin-panel/admin-panel.component";
 import {StudentPanelComponent} from "./main-panel/student/student-panel/student-panel.component";
 import {CoursePanelComponent} from "./main-panel/admin/admin-panel/course-panel/course-panel.component";
@@ -14,7 +13,6 @@ export const routes: Routes = [
   { path: '', redirectTo: '/start', pathMatch: 'full' },
   { path: 'start', component: StartComponent },
   { path: 'login', component: LoginComponent },
-  { path: 'register', component: RegisterComponent },
   { path: 'admin', component: AdminPanelComponent },
   { path: 'student', component: StudentPanelComponent },
   { path: 'login/:type', component: LoginComponent },
